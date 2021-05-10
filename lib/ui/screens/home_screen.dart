368d6e2279df5a72cb33cd323ea95dfcbbf7168d
frostrch <frostrch@gmail.com>
2021-05-10 14:12:24 +0300

    Fix Scroll, add buttons

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -5,6 +5,167 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   final ScrollController controller = ScrollController();
 
+  Widget get _appBar => Container(
+        color: Colors.white,
+        height: 80,
+        child: Row(
+          children: [
+            SizedBox(
+              width: 40,
+            ),
+            RoundedContainer(
+              height: 45,
+              width: 180,
+              color: Color(0xFFAA99FA),
+              contentPadding: EdgeInsets.zero,
+              child: Center(
+                child: Text(
+                  'Make an appointment',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget get _topContent => Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          RoundedContainer(
+            color: Colors.white,
+            width: 500,
+            height: 100,
+          ),
+          SizedBox(width: 25),
+          RoundedContainer(
+            color: Colors.deepPurple[300],
+            width: 300,
+            height: 100,
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Blood type:',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Alergies:',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Diseases:',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'AB+',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Penicilin, milk, peanuts',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Diabetes',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 12),
+          RoundedContainer(
+            color: Colors.deepOrange[300],
+            width: 300,
+            height: 100,
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Regular checkups',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'dr Anthony Wagner',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'dr Tom Humpton',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'dermatologist',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'dentist',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,52 +176,89 @@ class HomeScreen extends StatelessWidget {
           Expanded(
             child: Column(
               children: [
-                Container(
-                  color: Colors.white,
-                  height: 80,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 40,
-                      ),
-                      RoundedContainer(
-                        height: 45,
-                        width: 180,
-                        color: Color(0xFFAA99FA),
-                        contentPadding: EdgeInsets.zero,
-                        child: Center(
-                          child: Text(
-                            'Make an appointment',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                            ),
-                          ),
+                _appBar,
+                Expanded(
+                  child: Scrollbar(
+                    controller: controller,
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 30.0,
+                          horizontal: 40.0,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 30.0,
-                    horizontal: 40.0,
-                  ),
-                  child: Expanded(
-                    child: Scrollbar(
-                      controller: controller,
-                      child: SingleChildScrollView(
                         child: Column(
                           children: [
                             _topContent,
-                            SizedBox(
-                              height: 30,
+                            SizedBox(height: 20),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        'Medical history',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 23,
+                                        ),
+                                      ),
+                                      SizedBox(width: 20),
+                                      Text('Sort by type'),
+                                      SizedBox(width: 5),
+                                      Text('Consultation'),
+                                      SizedBox(width: 20),
+                                      Text('Sort by year'),
+                                      SizedBox(width: 5),
+                                      Text('2020'),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      RawMaterialButton(
+                                        onPressed: () {},
+                                        child: Text('Connect:'),
+                                      ),
+                                      RawMaterialButton(
+                                        onPressed: () {},
+                                        child: Text('Connect:'),
+                                      ),
+                                      RawMaterialButton(
+                                        onPressed: () {},
+                                        child: Text('Connect:'),
+                                      ),
+                                      RawMaterialButton(
+                                        onPressed: () {},
+                                        child: Text('Connect:'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
-                            DoctorCard(),
+                            SizedBox(height: 10),
+                            DoctorCard(
+                              child: Container(
+                                height: 300,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            DoctorCard(
+                              child: Container(
+                                height: 300,
+                              ),
+                            ),
                             SizedBox(
                               height: 10,
                             ),
-                            DoctorCard(),
+                            DoctorCard(
+                              child: Container(
+                                height: 300,
+                              ),
+                            ),
                           ],
                         ),
                       ),
@@ -74,160 +272,61 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-
-  Widget get _topContent {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        RoundedContainer(
-          color: Colors.white,
-          width: 500,
-          height: 100,
-        ),
-        SizedBox(width: 25),
-        RoundedContainer(
-          color: Colors.deepPurple[300],
-          width: 300,
-          height: 100,
-          child: Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Blood type:',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'Alergies:',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'Diseases:',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'AB+',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'Penicilin, milk, peanuts',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'Diabetes',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        SizedBox(width: 12),
-        RoundedContainer(
-          color: Colors.deepOrange[300],
-          width: 300,
-          height: 100,
-          child: Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Regular checkups',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'dr Anthony Wagner',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'dr Tom Humpton',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                width: 15,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'dermatologist',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'dentist',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
 }
 
-class DoctorCard extends StatelessWidget {
+class DoctorCard extends StatefulWidget {
+  DoctorCard({this.child});
+  final Widget child;
+
+  @override
+  _DoctorCardState createState() => _DoctorCardState();
+}
+
+class _DoctorCardState extends State<DoctorCard> {
+  bool isOpen = false;
+
   @override
   Widget build(BuildContext context) {
-    return RoundedContainer(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Icon(Icons.people),
-          Text('dr Name Surname'),
-          Text('Consultation'),
-          Text('Datetime'),
-          Text('Location'),
-          Text('Communication type'),
-          Text('Circle Button'),
-        ],
-      ),
+    return Column(
+      children: [
+        RoundedContainer(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.people),
+              Text('dr Name Surname'),
+              Text('Consultation'),
+              Text('Datetime'),
+              Text('Location'),
+              Text('Communication type'),
+              RawMaterialButton(
+                onPressed: () {
+                  setState(() {
+                    isOpen = !isOpen;
+                  });
+                },
+                elevation: 2.0,
+                fillColor:
+                    isOpen ? Colors.deepPurple[300] : Colors.deepPurple[100],
+                // padding: EdgeInsets.all(15.0),
+                shape: CircleBorder(),
+                child: Icon(
+                  isOpen ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down,
+                  size: 35.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+        if (isOpen) SizedBox(height: 10),
+        if (isOpen)
+          RoundedContainer(
+            color: Color(0xFFFAFAFA),
+            // height: 30,
+            width: double.infinity,
+            child: widget.child,
+          ),
+      ],
     );
   }
 }
